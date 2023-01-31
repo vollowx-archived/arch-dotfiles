86991745a04a07a6535ff5bd821730f31873cc5b
huge-pancake <huge.pancake.zxr@gmail.com>
2023-01-31 10:48:34 +0800

    NVIM | lualine styles

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -1,56 +1,60 @@
-local status_ok, lualine = pcall(require, 'lualine')
+local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
 
-local icons = require('user.icons')
+local icons = require("user.icons")
 
 local hide_in_width = function()
-  return vim.fn.winwidth(0) > 80
+	return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-  'diagnostics',
-  sources = { 'nvim_diagnostic' },
-  sections = { 'error', 'warn' },
-  symbols = {
-    error = icons.diagnostics.Error .. ' ',
-    warn = icons.diagnostics.Warning .. ' '
-  },
-  always_visible = true,
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = {
+		error = icons.diagnostics.Error .. " ",
+		warn = icons.diagnostics.Warning .. " ",
+	},
+	always_visible = true,
 }
 
 local diff = {
-  'diff',
-  colored = false,
-  symbols = {
-    added = icons.git.LineAdded .. ' ',
-    modified = icons.git.LineModified .. ' ',
-    removed = icons.git.LineRemoved .. ' '
-  }, -- changes diff symbols
-  cond = hide_in_width,
+	"diff",
+	colored = false,
+	symbols = {
+		added = icons.git.LineAdded .. " ",
+		modified = icons.git.LineModified .. " ",
+		removed = icons.git.LineRemoved .. " ",
+	}, -- changes diff symbols
+	cond = hide_in_width,
 }
 
 local spaces = function()
-  return 'spaces: ' .. vim.api.nvim_buf_get_option(0, 'shiftwidth')
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
-lualine.setup {
-  options = {
-    globalstatus = true,
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = icons.ui.DividerRight, right = icons.ui.DividerLeft  },
-    section_separators = { left = icons.ui.BoldDividerRight, right = icons.ui.BoldDividerLeft },
-    disabled_filetypes = { 'alpha', 'dashboard' },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = {'branch'},
-    lualine_c = { diagnostics, 'filename' },
-    lualine_x = { diff, spaces, 'encoding', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' },
-  },
-}
+lualine.setup({
+	options = {
+		globalstatus = true,
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = icons.ui.DividerRight, right = icons.ui.DividerLeft },
+		section_separators = { left = icons.ui.BoldDividerRight, right = icons.ui.BoldDividerLeft },
+		disabled_filetypes = { "alpha", "dashboard" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = {
+			{ "mode", separator = { left = icons.ui.BoldDividerLeft }, right_padding = 2 },
+		},
+		lualine_b = { "branch" },
+		lualine_c = { diagnostics, "filename" },
+		lualine_x = { diff, spaces, "encoding", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = {
+			{ "location", separator = { right = icons.ui.BoldDividerRight }, left_padding = 2 },
+		},
+	},
+})
