e4109481445d8d6b5394ec9d1657d479e7921e92
huge-pancake <huge.pancake.zxr@gmail.com>
2023-02-01 20:14:50 +0800

    FISH | add

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+source ~/.env
+
+if status --is-login
+  exec dbus-run-session Hyprland
+end
+
+alias ls="exa $argv"
+alias l="ls -a -l $argv"
+alias find="fd $argv"
+alias cat="bat $argv"
+alias dig="dog $argv"
+alias grep="rg $argv"
+alias df="duf $argv"
