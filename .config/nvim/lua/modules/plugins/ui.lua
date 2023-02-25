17e608143af7cbd8396581a8788946b2b9aa08b7
huge-pancake <huge.pancake.zxr@gmail.com>
2023-02-25 16:06:55 +0800

    NEOVIM | remove edge

--- .config/nvim/lua/modules/plugins/ui.lua
+++ .config/nvim/lua/modules/plugins/ui.lua
@@ -15,10 +15,6 @@ ui["catppuccin/nvim"] = {
 	name = "catppuccin",
 	config = require("ui.catppuccin"),
 }
-ui["sainnhe/edge"] = {
-	lazy = true,
-	config = require("ui.edge"),
-}
 ui["j-hui/fidget.nvim"] = {
 	lazy = true,
 	event = "BufReadPost",
