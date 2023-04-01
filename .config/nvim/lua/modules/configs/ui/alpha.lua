848d077e94abc8e5f3e81d375051bcc13650d532
huge-pancake <huge.pancake.zxr@gmail.com>
2023-04-01 23:21:03 +0800

    NEOVIM | update alpha

--- .config/nvim/lua/modules/configs/ui/alpha.lua
+++ .config/nvim/lua/modules/configs/ui/alpha.lua
@@ -55,47 +55,7 @@ return function()
 
 	local leader = " "
 	dashboard.section.buttons.val = {
-		button("space f c", "  Scheme change", leader, nil, {
-			noremap = true,
-			silent = true,
-			nowait = true,
-			callback = function()
-				require("telescope.builtin").colorscheme()
-			end,
-		}),
-		button("space f r", "  File frecency", leader, nil, {
-			noremap = true,
-			silent = true,
-			nowait = true,
-			callback = function()
-				require("telescope").extensions.frecency.frecency()
-			end,
-		}),
-		button("space f e", "  File history", leader, nil, {
-			noremap = true,
-			silent = true,
-			nowait = true,
-			callback = function()
-				require("telescope.builtin").oldfiles()
-			end,
-		}),
-		button("space f p", "  Project find", leader, nil, {
-			noremap = true,
-			silent = true,
-			nowait = true,
-			callback = function()
-				require("telescope").extensions.projects.projects({})
-			end,
-		}),
-		button("space f f", "  File find", leader, nil, {
-			noremap = true,
-			silent = true,
-			nowait = true,
-			callback = function()
-				require("telescope.builtin").find_files()
-			end,
-		}),
-		button("space f n", "  File new", leader, nil, {
+		button("space f n", "  New File", leader, nil, {
 			noremap = true,
 			silent = true,
 			nowait = true,
@@ -103,20 +63,52 @@ return function()
 				vim.api.nvim_command("enew")
 			end,
 		}),
-		button("space f w", "  Word find", leader, nil, {
+		button("space f c", "  Change Theme", leader, nil, {
+			noremap = true,
+			silent = true,
+			nowait = true,
+			callback = function()
+				require("telescope.builtin").colorscheme()
+			end,
+		}),
+		button("space f r", "  File Frecency", leader, nil, {
+			noremap = true,
+			silent = true,
+			nowait = true,
+			callback = function()
+				require("telescope").extensions.frecency.frecency()
+			end,
+		}),
+		button("space f e", "  File History", leader, nil, {
+			noremap = true,
+			silent = true,
+			nowait = true,
+			callback = function()
+				require("telescope.builtin").oldfiles()
+			end,
+		}),
+		button("space f p", "  Find Project", leader, nil, {
+			noremap = true,
+			silent = true,
+			nowait = true,
+			callback = function()
+				require("telescope").extensions.projects.projects({})
+			end,
+		}),
+		button("space f f", "  Find File", leader, nil, {
+			noremap = true,
+			silent = true,
+			nowait = true,
+			callback = function()
+				require("telescope.builtin").find_files()
+			end,
+		}),
+		button("space f w", "  Find Word", leader, nil, {
 			noremap = true,
 			silent = true,
 			nowait = true,
 			callback = function()
 				require("telescope.builtin").live_grep()
-			end,
-		}),
-		button("q", "  Quit", leader, "q", {
-			noremap = true,
-			silent = true,
-			nowait = true,
-			callback = function()
-				vim.cmd([[ quitall! ]])
 			end,
 		}),
 	}
