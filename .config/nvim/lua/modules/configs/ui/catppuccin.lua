df67f6e281de4de35e5c50f39bee7f89c048d938
huge-pancake <huge.pancake.zxr@gmail.com>
2023-04-16 17:09:38 +0800

    NEOVIM | no transparent background

--- .config/nvim/lua/modules/configs/ui/catppuccin.lua
+++ .config/nvim/lua/modules/configs/ui/catppuccin.lua
@@ -1,5 +1,5 @@
 return function()
-	local transparent_background = true -- Set background transparency here!
+	local transparent_background = false -- Set background transparency here!
 
 	require("catppuccin").setup({
 		flavour = "mocha", -- Can be one of: latte, frappe, macchiato, mocha
