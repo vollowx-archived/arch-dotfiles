dcb83f9912f76e7001943d77d6fd4758435f32f9
huge-pancake <huge.pancake.zxr@gmail.com>
2023-05-02 16:11:13 +0800

    NEOVIM | change nvim-tree keymap

--- .config/nvim/lua/modules/configs/tool/nvim-tree.lua
+++ .config/nvim/lua/modules/configs/tool/nvim-tree.lua
@@ -69,11 +69,7 @@ return function()
 		-- END_DEFAULT_ON_ATTACH
 
 		vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
-		vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
-		vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
 		vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
-		vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
-		vim.keymap.set("n", "H", api.node.open.horizontal, opts("Open: Horizontal Split"))
 	end
 
 	require("nvim-tree").setup({
